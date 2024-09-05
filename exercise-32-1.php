f03cea8ae73f5231b23480d77f58c3f954df8bad
Santiago Medina <samf0794@gmail.com>
2024-09-05 10:48:37 -0400

    ✨ Exercise 32
    exercise 32

---
+++ exercise-32-1.php
@@ -0,0 +1,28 @@
+<?php
+    $date = new DateTime($_POST['date']);
+    $day = $date->format('d');
+    $month = $date->format('m');
+    
+    function determineSeason($day, $month){
+        if($month >= 6 && $month <= 9){
+            if(($month == 6 && $day >= 21) || ($month == 9 && $day <= 21)){
+                return "Is summer!";
+            }
+        }
+        
+        return "Isn't summer!";
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 32-1</title>
+</head>
+<body>
+    <h1><?= determineSeason($day, $month) ?></h1>
+    <h4>Go back to <a href="exercise-32.php">Exercise 32</a></h4>
+</body>
+</html>
---
+++ exercise-32.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 32</title>
+</head>
+<body>
+    <form action="exercise-32-1.php" method="POST">
+        <label for="date">Date</label>
+        <input required type="date" name="date" id="date">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
