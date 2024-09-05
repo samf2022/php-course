93c2b753ee3d95e8c8b40b5bd122798d9e417130
Santiago Medina <samf0794@gmail.com>
2024-09-05 10:16:16 -0400

    exercise 30

---
+++ exercise-30-1.php
@@ -0,0 +1,26 @@
+<?php
+    $string_one = $_POST['string_one'];
+    $string_two = $_POST['string_two'];
+    $compare = strcasecmp($string_one, $string_two);
+    function compareStrings($compare){
+        if($compare == 0){
+            echo 'The strings are equal';
+        } else if($compare < 0){ 
+            echo 'The first string is less than the second';
+        } else {
+            echo 'The first string is greater than the second';
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= compareStrings($compare) ?></h1>
+    <h4>Go back to <a href="exercise-30.php">Exercise 30</a></h4>
+</body>
+</html>
---
+++ exercise-30.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 30</title>
+</head>
+<body>
+    <form action="exercise-30-1.php" method="POST">
+        <label for="string_one">String one</label>
+        <input type="text" name="string_one" id="string_one">
+
+        <label for="string_two">String two</label>
+        <input type="text" name="string_two" id="string_two">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
