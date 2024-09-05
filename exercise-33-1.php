ac950a7846e5fb16c2de6b4faca2318ddbd46748
Santiago Medina <samf0794@gmail.com>
2024-09-05 10:58:38 -0400

    ✨ Exercise 33
    Exercise 33

---
+++ exercise-33-1.php
@@ -0,0 +1,25 @@
+<?php
+$date = new DateTime($_POST['date']);
+$week_day = $date->format('w');
+
+function determineWeekend($week_day) {
+    if($week_day == 0 || $week_day == 6) {
+        return "Is the weekend";
+    } else {
+        return "Is a weekday";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 33-1</title>
+</head>
+<body>
+    <h1><?=determineWeekend($week_day)?></h1>
+    <h4>Go back to <a href="exercise-33.php">Exercise 33</a></h4>
+</body>
+</html>
---
+++ exercise-33.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 33</title>
+</head>
+<body>
+    <form action="exercise-33-1.php" method="POST">
+        <label for="date">Date</label>
+        <input required type="date" name="date" id="date">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
