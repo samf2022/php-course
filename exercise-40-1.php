5522d954cb958e6e880538bbb3e3e73ac88d5edb
Santiago Medina <samf0794@gmail.com>
2024-09-05 15:03:32 -0400

    ✨ Exercise 40

---
+++ exercise-40-1.php
@@ -0,0 +1,29 @@
+<?php
+$color = $_POST['color'];
+$number = $_POST['number'];
+
+function joinElements($color, $number){
+
+    $join = "";
+    if(isset($color) && isset($number)){
+        if($color != "" && $number != ""){
+            echo "Join List!! <br/>";
+            $join = "$color: $number";
+        }
+    }
+    return $join;
+    
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 40-1</title>
+</head>
+<body>
+    <h3><?= joinElements($color, $number) ?></h3>
+</body>
+</html>
---
+++ exercise-40.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="exercise-40-1.php" method="post">
+        <label for="color">Select Color:</label>
+        <select name="color" id="color">
+            <option value="">Color</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="yellow">Yellow</option>
+            <option value="black">Black</option>
+            <option value="white">White</option>
+        </select>
+        <br>
+        <label for="number">Enter a number:</label>
+        <input type="number" name="number" id="number" min="1" max="99">
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+<?php
+
+// Obtener el color seleccionado
+if (isset($_POST["color"])) 
+{
+    $color_seleccionado = $_POST["color"];
+} else {
+    $color_seleccionado = "";
+}
+
+// Obtener el número seleccionado
+if (isset($_POST["numero"])) 
+{
+    $numero_seleccionado = $_POST["numero"];
+} else {
+    $numero_seleccionado = "";
+}
+
+// Juntar las listas
+if ($color_seleccionado != "" && $numero_seleccionado != "") {
+    echo "<h3>Lista juntada</h3>";
+    echo "$color_seleccionado:$numero_seleccionado";
+}
+
+?>
+</html>
