69a160b939e5c005fdd8b04b72ac1de89cbe6b5d
Santiago Medina <samf0794@gmail.com>
2024-09-05 12:08:19 -0400

    ✨ Exercise 37
    exercise 37

---
+++ exercise-37-1.php
@@ -0,0 +1,21 @@
+<?php
+$number = $_POST['number'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 37-1</title>
+</head>
+<body>
+<?php
+    for($i = 1; $i <= $number; $i++) {
+        if($i % 2 == 0){
+            echo $i . "</br>";
+        }
+    }
+?>
+<h4>Go back to <a href="exercise-37.php">Exercise 37</a></h4>
+</body>
+</html>
---
+++ exercise-37.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 37</title>
+</head>
+<body>
+    <form action="exercise-37-1.php" method="POST">
+        <label for="number">Enter the number:</label>
+        <input required min="1" type="number" name="number" id="number">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
