616711df99b73797654741330b56bb60500b9ade
Santiago Medina <samf0794@gmail.com>
2024-09-05 11:59:42 -0400

    ✨ Exercise 35
    exercise 35

---
+++ exercise-35-1.php
@@ -0,0 +1,24 @@
+<?php
+ $original_date = new DateTime($_POST['date']);
+ $date = new DateTime($_POST['date']);
+ $day_numbers = $_POST['day_numbers'];
+ $resultDate = $date->modify('+' . $day_numbers . ' days');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 35-1</title>
+</head>
+<body>
+<h1>
+    <?= 
+        "The date is " . $original_date->format('Y-m-d') . "<br/>" .
+        "The day numbers entered are " . $day_numbers . "<br/>" .
+        "The result date is " . $resultDate->format('Y-m-d') . "\n"
+    ?>
+</h1>
+<h4>Go back to <a href="exercise-35.php">Exercise 35</a></h4>
+</body>
+</html>
---
+++ exercise-35.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 35</title>
+</head>
+<body>
+    <form action="exercise-35-1.php" method="POST">
+        <label for="date">Date</label>
+        <input required type="date" name="date" id="date">
+
+        <label for="day_numbers">Day numbers</label>
+        <input required type="number" name="day_numbers" id="day_numbers">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
