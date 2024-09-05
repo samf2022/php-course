78759ffae78e1be97c4cc264d0661a5cfde24022
Santiago Medina <samf0794@gmail.com>
2024-09-05 15:59:03 -0400

    🎨 Improve format
    improve format

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercises</title>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+        li + li {
+            margin-top:.5em;
+        }
+    </style>
 </head>
 <body>
     <h1>Exercises</h1>
